3becdbd9f2ff616241258c536707a030c98e5d77
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-27 12:51:47 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,5 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
+  # return the index of the value that was passed in
+  # If the value is not found, this method should return nil
 end
