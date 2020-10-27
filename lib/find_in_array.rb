7a04b2564621acd440b6ba407e204a8c5a958dc3
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-27 13:04:21 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -5,7 +5,7 @@ def find_element_index(array, value_to_find)
    counter = 0
    while array[counter] do
      if array[counter] == value_to_find
-       counter
+       return counter
      end
      counter += 1
    end
